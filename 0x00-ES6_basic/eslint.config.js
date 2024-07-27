7d39d7b67510751ad128e16b59498a50f6cd9177
BeygonK <kipkoechbeygon@gmail.com>
2024-07-27 11:36:00 +0300

    fixed lint

---
+++ 0x00-ES6_basic/eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.js
+import { ESLint } from 'eslint';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        node: true,
+        es2021: true,
+      },
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
+];
